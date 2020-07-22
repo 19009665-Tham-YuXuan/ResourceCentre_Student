6001736229855d7b953e0171d66fcf21d4f2a3ea
19015183 <19015183@DESKTOP-5PLTP8G>
2020-07-22 23:38:53 +0800

    (Most) Final Update!

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -199,6 +199,7 @@ public class ResourceCentreTest {
 		assertFalse("Test if Camcorder that is available cannot be returned", condition);
 		
 		//Test if loaned out Camcorder can be returned
+		ResourceCentre.addCamcorder(camcorderList, cc2);
 		cc2.setIsAvailable(false);
 		condition = ResourceCentre.doReturnCamcorder(camcorderList,"CC0012");
 		assertTrue("Test if loaned out Camcorder can be returned", condition);
@@ -221,6 +222,7 @@ public class ResourceCentreTest {
 		assertFalse("Test if Chromebook that is available cannot be returned", condition);
 		
 		//Test if loaned out Chromebook can be returned
+		ResourceCentre.addCamcorder(camcorderList, cb2);
 		cb2.setIsAvailable(false);
 		condition = ResourceCentre.doReturnChromebook(chromebookList,"CB0012");
 		assertTrue("Test if loaned out Chromebook can be returned", condition);
