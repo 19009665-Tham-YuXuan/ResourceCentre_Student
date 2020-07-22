a95a7478161dc3d075b60d3b176402cd8f59673a
19009665 <19009665@LAPTOP-UQT9M159>
2020-07-22 18:41:57 +0800

    update retrieveAllChromebookTest()

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -116,8 +116,8 @@ public class ResourceCentreTest {
 		// from the SourceCentre
 		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
 
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB001", "ASUS Chromebook", "Win 10", "Yes","");
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB002", "HP Chromebook", "Win 10", "Yes","");
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Mac OS", "Yes","");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Win 10", "Yes","");
 
 		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
