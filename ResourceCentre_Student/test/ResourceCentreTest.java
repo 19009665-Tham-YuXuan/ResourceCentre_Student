b7d1a5d1d029c1553c940245a8a64ad878b6d6ee
19015183 <19015183@DESKTOP-5PLTP8G>
2020-07-22 20:05:48 +0800

    Fix Error 5

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -116,9 +116,8 @@ public class ResourceCentreTest {
 		// from the SourceCentre
 		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
 		
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Mac OS", "Yes", "");
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Win 10", "Yes", "");
-
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "Yes", "My Google Chromebook 1st", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "Yes", "SAMSUNG Chromebook 4+", "", "Win 10");
 		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
 
