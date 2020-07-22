58cfd3724ee8915ff105e05ff6d18eac5541fefa
19009665 <19009665@LAPTOP-UQT9M159>
2020-07-22 14:05:10 +0800

    update viewAllChromebood() in ResourCentre

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -84,7 +84,7 @@ public class ResourceCentreTest {
 		// Test if Item list is not null but empty, so that can add a new item
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", camcorderList);
 		
-		//test if the list of chromebook retrieved from the SourceCentre is empty
+		//test if the list of chrmebook retrieved from the SourceCentre is empty
 				String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 				String testOutput = "";
 				assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
